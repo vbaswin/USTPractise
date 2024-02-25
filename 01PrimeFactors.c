20095dd6d8910d6f97dc7159d77aea2c796c7f8c
vbaswin <aswinvb10a@gmail.com>
2024-02-25 21:34:34 +0530

    made the prime function simpler to understand

--- 01PrimeFactors.c
+++ 01PrimeFactors.c
@@ -12,13 +12,13 @@
 
 int prime(int n) {
 	if (n <= 1) return 0;
-	if (n <= 3) return 1;
+	if (n == 2) return 1;
 
-	if (n % 2 == 0 || n % 3 == 0) return 0;
-
-	for (int i = 5; i * i <= n; i = i + 6)
-		if (n % i == 0 || n % (i + 2) == 0)
+	for (int i = 2; i < n; ++i) {
+		if (n % i == 0)
 			return 0;
+	}
+
 
 	return 1;
 }
